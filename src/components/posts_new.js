777870f1e0a776d56cfbd4f3dcedc0894bd2cdc7
inkymaze <nickwcannon@gmail.com>
2017-08-31 15:14:40 -0700

    add switch router and new post link

---
+++ src/components/posts_new.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+
+
+  render () {
+    return (
+      <div>
+        Posts new
+      </div>
+
+    );
+  }
+}
+
+export default PostsNew;
