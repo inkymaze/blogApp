0e6c00deb803ce120728a6119d6289d3f1c9d337
inkymaze <nickwcannon@gmail.com>
2017-08-31 16:48:13 -0700

    add validation to new post form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -5,10 +5,22 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
 
-  renderTitleField(field){
+  // the 'on' handlers are replaced by the ...field below
+  // / onChange={field.input.onChange}
+  // onFocus={field.input.onFocus}
+  // field is a specified slice of state
+  // meta.error looks at that same 'name' attribute and
+  //  passes on errors for that tag if they exist
+
+  renderField(field){
     return (
-      <div>
-        <input />
+      <div className='form-group'>
+        <label>{field.label}</label>
+        <input className='form-control'
+          type='text'
+          {...field.input}
+        />
+        {field.meta.error}
       </div>
     );
   }
@@ -17,19 +29,46 @@ class PostsNew extends Component {
   // to render JSX to the DOM
   render () {
     return (
-      <form>
+      <form >
         <Field name="title"
-               component={this.renderTitleField}
+              label='Title for Post'
+               component={this.renderField}
         />
+        <Field name="categories"
+              label='Categories'
+               component={this.renderField}
+        />
+        <Field name="content"
+              label='Post Content'
+               component={this.renderField}
+        />
+        <button type='submit' className='btn btn-primary'>Submit</button>
       </form>
-
     );
   }
 }
 
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Enter a title!";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter a categories!";
+  }
+  if (!values.content) {
+    errors.content = "Enter a content!";
+  }
+  // if errors is empty then form is fine to submit
+  // else redux-form assumes form is valid
+  return errors;
+}
+
 // below helps component communicate to the reducer that has already been setup
 // below specifies a namespace for the app state
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
