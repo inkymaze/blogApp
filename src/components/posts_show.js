c1f882a2137ad38716c50173e73bb345798c74c1
inkymaze <nickwcannon@gmail.com>
2017-09-05 17:51:55 -0700

    add delete post functionality

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 
 
 class PostsShow extends React.Component {
   componentDidMount() {
     // the If condition makes it easier on network usage if the details are called from the
-    // index page already 
+    // index page already
     if (!this.props.post) {
     // pulls the id out of params
     const { id } = this.props.match.params;
     this.props.fetchPost(id);
     }
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
+
   }
 
 
@@ -24,6 +32,12 @@ class PostsShow extends React.Component {
     return (
       <div>
         <Link to="/">Back To Index</Link>
+
+        <button className='btn btn-danger pull-xs-right'
+          onClick={this.onDeleteClick.bind(this)}
+          >
+          DELETE POST 
+        </button>
         <h2>{post.title}</h2>
         <h5>Categories: {post.categories}</h5>
         <p>{post.content}</p>
@@ -40,4 +54,4 @@ function mapStateToProps({ posts }, ownProps) {
   return {post: posts[ownProps.match.params.id]};
 }
 
-export default connect(mapStateToProps,{ fetchPost })(PostsShow);
+export default connect(mapStateToProps,{ fetchPost, deletePost })(PostsShow);
