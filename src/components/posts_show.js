cff56da08748823f842d763107cba878e19983f4
inkymaze <nickwcannon@gmail.com>
2017-09-05 17:41:37 -0700

    navigation to and from post show pages

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPost } from '../actions';
 
 
 class PostsShow extends React.Component {
   componentDidMount() {
+    // the If condition makes it easier on network usage if the details are called from the
+    // index page already 
+    if (!this.props.post) {
     // pulls the id out of params
     const { id } = this.props.match.params;
-    this.props.fetchPost();
+    this.props.fetchPost(id);
+    }
   }
 
 
   render () {
+    const { post } = this.props;
+
+    if (!post) { return <div>Loading...</div>;}
+
     return (
       <div>
-        Posts Show!
+        <Link to="/">Back To Index</Link>
+        <h2>{post.title}</h2>
+        <h5>Categories: {post.categories}</h5>
+        <p>{post.content}</p>
       </div>
     );
   }
